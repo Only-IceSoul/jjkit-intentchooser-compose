60dc73ddcf10242636e5d891654806187e4c7f13
Juan <justinjlf21@gmail.com>
2024-07-08 00:50:17 -0400

    builder ver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+//        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
 }
 
