363d67054c51a735c16d78ae38976f6bc3abede3
Juan <justinjlf21@gmail.com>
2024-07-08 01:14:12 -0400

    ver test 2

--- intentchooser/build.gradle.kts
+++ intentchooser/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
