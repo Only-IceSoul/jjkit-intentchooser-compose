683dda4652457a694aca0551d8cd18d9ff11d8cf
Juan <justinjlf21@gmail.com>
2024-07-08 01:08:15 -0400

    ver bb

--- intentchooser/build.gradle.kts
+++ intentchooser/build.gradle.kts
@@ -45,7 +45,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
